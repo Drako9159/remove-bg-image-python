7e73407089fcef99d8d123dbe2e5c2c7ba335d13
drako9159 <chochito9159@gmail.com>
2024-12-20 15:18:15 -0600

    feat(files): configure paths

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import sys
 from datetime import datetime
 from rembg import remove
+
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -14,11 +14,12 @@ class BackgroundRemove:
     def process_images(self):
         today = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
         proccessed_folder = os.path.join(self.output_path, today)
-        os.makedirs(proccessed_folder, exist_ok=True)
+        
         for filename in os.listdir(self.input_path):
-            if filename.endswith(('.png', 'jpg', 'jpeg')):
+            if filename.endswith(('.png', 'jpg', 'jpeg', 'webp')):
+                os.makedirs(proccessed_folder, exist_ok=True)
                 input_path = os.path.join(self.input_path, filename)
-                output_path = os.join(proccessed_folder, filename)
+                output_path = os.path.join(proccessed_folder, filename)
                 self.remove_background(input_path, output_path)
                 self.save_images(input_path, proccessed_folder)
 
@@ -36,4 +37,7 @@ class BackgroundRemove:
         new_path = os.path.join(originals_path, filename)
         os.rename(input_file, new_path)
 
+if __name__ == '__main__':
+    bg_remove = BackgroundRemove("input", "output")
+    bg_remove.process_images()
         
